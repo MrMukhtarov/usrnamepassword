73e408f563e8ebc7a7d4161bfb19e2343c084ffd
MrMukhtarov <nijattm@gmail.com>
2022-04-12 02:19:43 +0400

    git commit -m

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HomeworkClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("12345678");
+            user.Password = "12345678";
+            Console.WriteLine(user.Password);
+            
+            
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkClass
+{
+
+    class User
+    {
+        private string _userName;
+        private string _password;
+        
+        
+
+        
+        
+ 
+       public string UserName
+        {
+            get
+            {
+                return _userName;
+            }
+            set
+            {
+                if (value.Length >= 8)
+                {
+                    _userName = UserName;
+                }
+                else
+                {
+                    Console.WriteLine("Duzgun daxil et");
+                }
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if(value.Length >= 8)
+                {
+                    
+                    _password = Password;
+                    foreach (char item in _password)
+                    {
+                        if (char.IsUpper(item))
+                        {
+                            
+                        }
+                    }
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Yeniden daxil edin");
+                }
+            }
+        }
+
+            public User(string Username, string Password)
+            {
+              _userName = Username;
+              
+            }
+            public User(string Password)
+            {
+                _password = Password;
+            }
+        
+    }
+}
